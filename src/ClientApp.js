9956c42a66206c7f6b4d94af57bfd8d5861a9831
Leon-Mbegera <mbegeraleon89@gmail.com>
2022-05-10 11:36:58 +0300

    Use hydrate to tell react to utilize what's already built rather than build from scratch suing render

--- src/ClientApp.js
+++ src/ClientApp.js
@@ -1,8 +1,8 @@
-import { render } from "react-dom";
+import { hydrate } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-render(
+hydrate(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
